67d6fcfa393ce3a568586c537ecc967a09dcbf64
thatguy <thatguy@yandex-team.ru>
2020-02-10 23:50:31 +0300

    add termination exit on incorrect user input

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	const rubToUsdCourse float64 = 62.34
@@ -8,6 +11,7 @@ func main() {
 	fmt.Println("Сколько рублей у вас есть?")
 	if _, err := fmt.Scanln(&rubAmount); err != nil {
 		fmt.Println("Вы ввели не число...")
+		os.Exit(1)
 	}
 	fmt.Printf("У вас есть %.2f рублей\n", rubAmount)
 	fmt.Printf("В долларах это стоставляет: %.2f долларов\n", rubAmount/rubToUsdCourse)
