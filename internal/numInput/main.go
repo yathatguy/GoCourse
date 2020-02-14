ea302a838238f9eee6f4394fe9648b8521b0deef
thatguy <thatguy@yandex-team.ru>
2020-02-14 12:24:10 +0300

    add internal dir for internal packages

---
+++ internal/numInput/main.go
@@ -0,0 +1,51 @@
+package numInput
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func inputData(msg string) (data string){
+	fmt.Print(msg)
+	if _, err := fmt.Scanln(&data); err != nil {
+		log.Println("input error: ", err)
+	}
+	if data == "exit" {
+		panic(nil)
+	}
+	return
+}
+
+func EnterIntNumber(msg string) int {
+	for {
+		num, err := strconv.ParseFloat(inputData(msg), 32)
+		if err == nil {
+			// Ввод числа прошел без ошибок
+			return int(num)
+		}
+		log.Fatalln("unable to parse input")
+	}
+}
+
+func EnterFloat32Number(msg string) float32 {
+	for {
+		num, err := strconv.ParseFloat(inputData(msg), 32)
+		if err == nil {
+			// Ввод числа прошел без ошибок
+			return float32(num)
+		}
+		log.Fatalln("unable to parse input")
+	}
+}
+
+func EnterFloat64Number(msg string) float64 {
+	for {
+		num, err := strconv.ParseFloat(inputData(msg), 32)
+		if err == nil {
+			// Ввод числа прошел без ошибок
+			return num
+		}
+		log.Fatalln("unable to parse input")
+	}
+}
