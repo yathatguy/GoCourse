6b8edfe97e3e85fa30247ca80c25c2b6a78d5ec2
thatguy <thatguy@yandex-team.ru>
2020-02-14 14:27:17 +0300

    write addressBook to json file

--- cmd/syntax_task3/addressBook/main.go
+++ cmd/syntax_task3/addressBook/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
 func main() {
-	$END$
+	addressBook := make(map[string][]int)
+
+	addressBook["Alex"] = []int{89996543210}
+	addressBook["Bob"] = []int{89167243812}
+	addressBook["Bob"] = append(addressBook["Bob"], 89155243627)
+
+	fmt.Println(addressBook)
+
+	for name, numbers := range addressBook {
+		fmt.Println("Абонент:", name)
+		for i, number := range numbers {
+			fmt.Printf("\t %v) %v \n", i+1, number)
+		}
+	}
+
+	addBookJson, err := json.Marshal(addressBook)
+	if err != nil {
+		fmt.Printf("Ошибка конвертации в json: %v\n", err)
+	}
+
+	err = ioutil.WriteFile("addBookJson.json", addBookJson, 0644)
+	if err != nil {
+		fmt.Printf("Ошибка записи в файл: %v\n", err)
+	}
 }
