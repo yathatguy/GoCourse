fb2c62cf0e0c1b26b5747afade423c918ccc9449
thatguy <thatguy@yandex-team.ru>
2020-02-25 21:23:37 +0300

    rewrite simpeNum task

--- cmd/syntax_task2/simpleNumbers/main.go
+++ cmd/syntax_task2/simpleNumbers/main.go
@@ -2,45 +2,32 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"math"
 )
 
 func main() {
-	arr := createArray(1000)
-	simpleArr := getSimpleArray(arr)
-	fmt.Println(simpleArr)
+	findSimple(100)
 }
 
-func createArray(l int) []int {
-	resArr := make([]int, l, l)
-	if l < 1 {
-		fmt.Printf("  unable to create array with %v in length\n", l)
-		os.Exit(1)
-	}
-	for i := 1; i <= l; i++ {
-		resArr[i] = i
-	}
-	return resArr
-}
-
-func getSimpleArray(arr []int) []int {
-	simpleArr := make([]int, len(arr), len(arr))
-	for i := 1; i < len(arr); i++ {
-		for j := i+1; j < len(arr); j++ {
-			if arr[j] != 0 && arr[i] != 0{
-				if arr[j] % arr[i] == 0 {
-					arr[j] = 0
+func findSimple(n int) {
+	foundCnt := 0
+	isDividable := false
+	for i := 1; foundCnt < n; i++ {
+		switch i {
+		case 1:
+			fmt.Println(1)
+		default:
+			isDividable = false
+			for j := 2; j <= int(math.Sqrt(float64(i))); j++ {
+				if i % j == 0 {
+					isDividable = true
+					break
 				}
+			}
+			if !isDividable {
+				foundCnt++
+				fmt.Println(i)
 			}
 		}
 	}
-	var j = 0
-	for i := 0; i < len(arr); i++ {
-		if arr[i] != 0 {
-			simpleArr[j] = arr[i]
-			j += 1
-		}
-	}
-	// TODO: resArr should not be fixed length, it can be constructed with blocks, each new block adds if getSimpleArray is not fit to target length
-	return simpleArr[:100]
 }
