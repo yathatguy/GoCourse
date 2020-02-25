c5af32fc7c697f48c29788f22a94684e6bb03075
thatguy <thatguy@yandex-team.ru>
2020-02-25 20:39:58 +0300

    rewrite fibonacci task without array usage

--- cmd/syntax_task2/fibonacci/main.go
+++ cmd/syntax_task2/fibonacci/main.go
@@ -6,13 +6,23 @@ import (
 )
 
 func main() {
-	fibRow := make([]*big.Int, 100, 100)
+	var (
+		prevMinusOne = big.NewInt(0)
+		prev = big.NewInt(0)
+		fibNum = big.NewInt(0)
+	)
 	for i := 0; i<100; i++ {
 		switch i {
-		case 0: fibRow[i] = big.NewInt(0)
-		case 1: fibRow[i] = big.NewInt(1)
-		default: fibRow[i] = big.NewInt(0).Add(fibRow[i-1], fibRow[i-2])
+		case 0:
+			fibNum = big.NewInt(0)
+		case 1:
+			fibNum = big.NewInt(1)
+			prev = fibNum
+		default:
+			fibNum = big.NewInt(0).Add(prev, prevMinusOne)
+			prevMinusOne = prev
+			prev = fibNum
 		}
-		fmt.Printf("%v число в последовательности Фибоначчи: %v\n", i+1, fibRow[i])
+		fmt.Printf("%v число в последовательности Фибоначчи: %v\n", i+1, fibNum)
 	}
 }
