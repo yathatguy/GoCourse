072dc702e2ac36452f1ea8abbbee922993e20e72
thatguy <thatguy@yandex-team.ru>
2020-03-04 22:11:36 +0300

    find ubuntu mirror ip

--- cmd/syntax_task7/bufChannels/main.go
+++ cmd/syntax_task7/bufChannels/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
-	$END$
+	ips := mirroredQuery()
+	fmt.Printf("The nearest Ubuntu mirrow for you in Russian Federartion has the following IP(s): %v", ips)
+}
+
+func mirroredQuery() []string {
+	responses := make(chan []string, 3)
+
+	for _, fqdn := range []string{"mirrors.powernet.com.ru", "mirror.yandex.ru", "mirror.timeweb.ru", "mirror.corbina.net", "mirror.beget.ru", "mirror.truenetwork.ru"} {
+		go func(fqdn string) {
+			ips, err := request(fqdn)
+			if err != nil {
+				log.Printf("can't get IP address for %s", fqdn)
+			}
+			responses <- ips
+		}(fqdn)
+	}
+	return <-responses
+}
+
+func request(hostname string) ([]string, error) {
+	ips, err := net.LookupIP(hostname)
+	if err != nil {
+		return nil, errors.New("can't get IP address")
+	}
+	var ipSlice []string
+	for _, ip := range ips {
+		ipSlice = append(ipSlice, ip.String())
+	}
+	return ipSlice, nil
 }
